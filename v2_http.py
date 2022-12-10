07d339af319fdd3b70b47a3b1cc8dafe55ede4e6
Aislin <72391548+Xuanluo-Qiu@users.noreply.github.com>
2022-12-10 18:01:32 +0800

    Update v2_http.py

--- v2_http.py
+++ v2_http.py
@@ -10,7 +10,11 @@ class HTMLParser_v2(HTMLParser):
 api = "https://v2.api.light.xhhzs.cn/v2/"
 
 def get_announcement():
-    get_data = requests.get(api+"announcement")
+    try:
+        get_data = requests.get(api+"announcement")
+    except KeyError:
+        print("错误:您可能没有连接网络或网络超时")
+        exit()
     json_data = get_data.json()
     announcement = json_data["data"]
     return announcement
@@ -28,7 +32,11 @@ def get_read_post(post_id):
     get_data = requests.get(api+"post/single?id="+post_id)
     json_data = json.loads(get_data.text)
     text_data = json_data.get("data")
-    text_title = text_data.get("title")
+    try:
+        text_title = text_data.get("title")
+    except AttributeError:
+        print("错误:无法获取到文章数据,请检查输入是否存在\n")
+        return 1
     text_body = text_data.get("data")
     parser = HTMLParser_v2()
     print(f"\n{text_title}\n")
